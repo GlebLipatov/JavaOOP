6fc5fc52672f275479c1dad307dab1173d79420e
Gleb Lipatov <it.lipatov@gmail.com>
2023-07-14 15:31:43 +0300

    bag

---
+++ src/HomeWorks/HW2/Classes/Actor.java
@@ -0,0 +1,13 @@
+package HomeWorks.HW2.Classes;
+
+public abstract class Actor {
+    protected String name;
+    protected boolean isTakeOrder;
+    protected boolean isMakeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    abstract public String getName();
+}
